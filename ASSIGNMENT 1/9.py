c3595e69d0fddff1cf1ddce0e6705c107bef4955
JIYA_GUPTA_MCA_A_2024-26_23_UEMK <jiyag0057@gmail.com>
2025-03-06 00:09:21 +0530

    Create 9.py

---
+++ ASSIGNMENT 1/9.py
@@ -0,0 +1,9 @@
+Write a Python program to count number of node present in a given binary tree using
+array.
+
+def count_nodes_in_array(arr):
+return len(arr)
+
+# Example usage
+arr = [1, 2, 3, 4]
+print("Number of nodes in the tree:", count_nodes_in_array(arr))
